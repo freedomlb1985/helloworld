501b679ea5c3cf7d0dc4ac264298e30b5d6952b4
frey <frey.gu@chinapex.com>
2016-07-12 10:50:46 +0800

    new branch

--- src/activity/models/activity.py
+++ src/activity/models/activity.py
@@ -1,9 +1,8 @@
 from django.db import models
-from account.models import Account
 
 class AccountActivity(models.Model):
 #     email = models.EmailField(unique=True)
-    account = models.ForeignKey(Account, unique=False)
+    account = models.CharField(max_length = 25)
     activity = models.CharField(max_length = 25)
     type = models.CharField(max_length = 25)
     typename = models.CharField(max_length = 100)
@@ -19,7 +18,7 @@ class AccountActivity(models.Model):
     
 class ContainerActivity(models.Model):
 #     email = models.EmailField(unique=True)
-    account = models.ForeignKey(Account, unique=False)
+    account = models.CharField(max_length = 25)
     activity = models.CharField(max_length = 25)
     type = models.CharField(max_length = 25)
     typename = models.CharField(max_length = 100)
@@ -27,9 +26,16 @@ class ContainerActivity(models.Model):
     ipaddress = models.CharField(max_length = 15)
     class Meta:
         db_table = "container_activity"
+    def save(self, force_insert=False, force_update=False, using=None, 
+        update_fields=None):
+        print 'save'
+        if True:
+            return
+        return models.Model.save(self, force_insert=force_insert, force_update=force_update, using=using, update_fields=update_fields)
         
         
-        
+if __name__ == '__main__':
+    ContainerActivity.objects.create()
         
         
         
--- src/com/chinapex/main/HelloWorld.py
+++ src/com/chinapex/main/HelloWorld.py
@@ -2,6 +2,9 @@ import sys
 
 import com.chinapex.libs.MyLibs as mylibs
 from com.chinapex.aop.DecoratorDemo import deco
+import logging
+
+logger = logging.getLogger('django_debug')
 
 sys.stderr.write("import test!\n")
 sys.stdout.write("import test!\n")
@@ -36,12 +39,36 @@ class FooClass:
         return (self.name, self.age, self.height, self.weight)
     
     
+def test1(instance = None, data = None, **kwargs):
+    print instance
+    print data
+    print kwargs
+    
     
 
 if __name__ == '__main__':
-    foo = FooClass('kobe', 39, 198, 100);
-    foo.showMe()
-    print foo
+#     test1(instance = 'instance', data1 = "data",ss = 111,k = 222)
+#     test1('instance', 'data',a = '1111')
+#     a = {}
+#     b = a.pop('a' , None)
+#     print b
+#     if 3> 1:
+#         print '11111111111'
+#     elif 2>1:
+#         print '222222222222222'
+#     else:
+#         print '333333333333333'
+      print 'MY_creae'.find('create')
+      print 'MY_create'.find('update')
+      if 'MY_creae'.find('create'):
+          print 'create'
+      elif 'MY_update'.find('up'):
+          print 'update'
+      else:
+          print 'aaaa'
+#     foo = FooClass('kobe', 39, 198, 100);
+#     foo.showMe()
+#     print foo
 #     info = foo.getInformation()
 #     print info
 #     print sys.path
